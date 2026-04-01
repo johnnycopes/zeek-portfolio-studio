12d4c0e410e29c019f8dff38728a76c6508e0469
John Coppola <jdcoppola@gmail.com>
2026-04-01 09:51:21 -0400

    TOOL: format on save

--- sanity.config.ts
+++ sanity.config.ts
@@ -5,7 +5,7 @@ import {schemaTypes} from './schemaTypes'
 
 export default defineConfig({
   name: 'default',
-  title: 'Zack\'s Portfolio',
+  title: "Zack's Portfolio",
 
   projectId: 'wasxg3b6',
   dataset: 'production',
